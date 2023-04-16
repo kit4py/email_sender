69c75bd34be5c08989945abb15d4462f4dec5099
kit4py <105520646+kit4py@users.noreply.github.com>
2023-04-16 13:54:28 +0200

    Add files via upload

---
+++ email_sender.py
@@ -0,0 +1,37 @@
+# email sender
+# you can use it as a email marketing tool
+# don't forget to customize the html file
+
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+print('''
+█▀▀ █▀▄▀█ ▄▀█ █ █░░ █▀ █▀▀ █▄░█ █▀▄ █▀▀ █▀█
+██▄ █░▀░█ █▀█ █ █▄▄ ▄█ ██▄ █░▀█ █▄▀ ██▄ █▀▄
+''')
+
+# your email list
+email_list = ['nkhr.work@gmail.com',
+              'email@1.com',
+              'email@2.com'
+              ]
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()  # connect securely to the server
+    smtp.login('your_email', 'your_password')
+
+    for mail in email_list:
+        html = Template(Path('index.html').read_text())
+        email = EmailMessage()
+        email['from'] = 'your name'
+        email['to'] = mail
+        email['subject'] = 'your Subject'
+        # you can create a dict with users and send each one an individual mail
+        email.set_content(html.substitute({'name': 'User1'}), 'html')
+        print(f'sent to {mail}')
+
+        smtp.send_message(email)
+    print('all completed!')
